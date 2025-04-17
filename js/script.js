bf4d5f61892f89c24af5a62f5a11b3f6ae069e31
Jon <jonathancalesii@gmail.com>
2025-04-16 22:49:19 -0400

    js added for modal functionality

---
+++ js/script.js
@@ -0,0 +1,78 @@
+document.addEventListener("DOMContentLoaded", function() {
+// As I understand it, modals are comprised of three major parts:
+
+// The modal container, what will be opened.
+let modalContainer = {
+    albumContainer: document.getElementById("albumContainer"),
+    sfContainer: document.getElementById("sfContainer"),
+    personalContainer: document.getElementById("personalContainer"),
+}
+
+// The modal button, what trigger the opening.
+let modalButton = {
+    albumButton: document.getElementById("albumButton"),
+    sfButton: document.getElementById("sfButton"),
+    personalButton: document.getElementById("personalButton")
+}
+
+// The modal span, that will close the modal.
+let modalClose = {
+    albumClose: document.getElementById("albumSpan"),
+    sfClose: document.getElementById("sfSpan"),
+    personalClose: document.getElementById("personalSpan")
+}
+
+// For the sake of intuative design, Im going to make an object
+// to recognize and hold the body objects of the modals so that
+// JS can determine if the user ever clicks outside of them later
+// for a quick exit.
+
+let modalContent = {
+    albumBody: document.getElementById("albumBody"),
+    sfBody: document.getElementById("sfBody"),
+    personalBody: document.getElementById("personalBody"),
+}
+
+// Open function for the personalButton
+modalButton.personalButton.onclick = function(){
+    modalContainer.personalContainer.style.display = 'block';
+}
+
+// Open function for the albumButton
+modalButton.albumButton.onclick = function(){
+    modalContainer.albumContainer.style.display = 'block';
+}
+
+// Open function for the sfButton
+modalButton.sfButton.onclick = function(){
+    modalContainer.sfContainer.style.display = 'block';
+}
+
+// Close function for the personalSpan
+modalClose.personalClose.onclick = function(){
+    modalContainer.personalContainer.style.display = 'none';
+}
+
+
+// Close function for the albumSpan
+modalClose.albumClose.onclick = function(){
+    modalContainer.albumContainer.style.display = 'none';
+}
+
+// Close function for the sfSpan
+modalClose.sfClose.onclick = function(){
+    modalContainer.sfContainer.style.display = 'none';
+}
+
+// Auto-close for the modals if user clicks out of bounds.
+window.onclick = function(event) {
+    // Loop through each modal container
+    for (let key in modalContainer) {
+        let modal = modalContainer[key];
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    }
+}
+
+});
